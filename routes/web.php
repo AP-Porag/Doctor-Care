f2fefd9ccd23a1457e01a4d9388e67d857976c00
AP-Porag <smauhp@gmail.com>
2020-11-30 01:45:22 +0600

    Set initial route for Website Front-end

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 //Route::get('/', function () {
 //    return view('welcome');
 //});
 
+
+//front end rute start
 Route::get('/', function () {
     return view('layouts.website');
-});
+})->name('website');
 
 
 Auth::routes();
 
+//backend route start
 Route::get('/home', 'HomeController@index')->name('home');
