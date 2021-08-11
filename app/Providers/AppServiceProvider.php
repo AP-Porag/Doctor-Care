dc8366bd3f94bafbb6a62bd8791852d7c4e10618
AP-Porag <smauhp@gmail.com>
2021-08-11 20:31:56 +0600

    Fixed the HTTP security Issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //check the HTTP request to determine if the app is on local or production mode
+
+        if (env('APP_ENV') !== 'local'){
+            URL::forceScheme('https');
+        }
     }
 }
